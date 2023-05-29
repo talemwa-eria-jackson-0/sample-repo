0145450c94817b55d62424d34b3632fe4a9075c9
talemwa-eria-jackson-0 <talemwaeriajackson0@gmail.com>
2023-05-29 12:08:49 +0300

    Just added these 2 files

---
+++ ch2Sample1.java
@@ -0,0 +1,14 @@
+/* program to display a java window*/
+
+import javax.swing.*;
+
+class Ch2Sample1 {
+	public static void main(String[] args) {
+		JFrame myWindow;
+		myWindow = new JFrame();
+		myWindow.setSize(300, 200);
+		myWindow.setTitle("My first java program");
+		myWindow.setVisible(true);
+	}
+}
+
---
+++ hello-world.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+    	int y = 69;
+    	int x = 78;
+        System.out.println("Hello, World!");
+        System.out.println(x*y);
+    }
+
+}
+
